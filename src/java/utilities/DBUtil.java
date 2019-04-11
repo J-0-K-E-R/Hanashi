0f5632b536876cc680b2f21b4e3bb36a461bd36b
J-0-K-E-R <aman1334@yahoo.com>
2019-04-11 21:45:38 +0530

    Added close connection for PreparedStatement in utilities.DBUtil

--- src/java/utilities/DBUtil.java
+++ src/java/utilities/DBUtil.java
@@ -6,6 +6,7 @@
 package utilities;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,7 +26,6 @@ public class DBUtil {
         }
     }
 
-
     public static void close(Statement st) {
         try {
             if (st != null) {
@@ -36,6 +36,16 @@ public class DBUtil {
         }
     }
 
+    public static void close(PreparedStatement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
     public static void close(ResultSet rs) {
         try {
             if (rs != null) {
